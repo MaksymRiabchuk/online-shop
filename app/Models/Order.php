ac1592f5307ad5cf15a265154a7703b5a8613ed3
RomanH <hromanhonchar2004@gmail.com>
2025-01-30 20:23:16 +0100

    Updated models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,15 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Order extends Model
 {
     use HasFactory;
 
-    public function profile() : BelongsTo
+    public function user() : BelongsTo
 
     {
-        return $this->belongsTo(Profile::class);
+        return $this->belongsTo(User::class);
     }
+
+    protected $fillable = ['user_id', 'name', 'lastname', 'phone', 'email', 'status', 'comment',];
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'lastname',
+        'phone',
         'email',
         'password',
     ];
